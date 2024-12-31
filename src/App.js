887a4c4d0882661f27b27594516328be6411e5c7
shubhamrsaroj <shubhamrsaroj229@gmail.com>
2024-12-31 18:35:58 +0530

    added local storage in it

--- src/App.js
+++ src/App.js
@@ -1,74 +1,71 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NotesList from "./components/NotesList";
-import {nanoid} from 'nanoid';
+import { nanoid } from 'nanoid';
 import Search from "./components/Search";
 import Header from "./components/Header";
-const App=()=>{
 
+const App = () => {
+    const [notes, setNotes] = useState(() => {
+       
+        const savedNotes = JSON.parse(localStorage.getItem('react-notes-app-data')) || [
+            {
+                id: nanoid(),
+                text: "hello brother",
+                date: "12/5/34"
+            },
+            {
+                id: nanoid(),
+                text: "hello jiju",
+                date: "12/5/34"
+            },
+            {
+                id: nanoid(),
+                text: "hello saala",
+                date: "12/5/34"
+            }
+        ];
+        return savedNotes;
+    });
 
+    const [search, setSearch] = useState("");
 
-  const[notes,setNotes]=useState([
-     {
-      id:nanoid(),
-       text:"hello brother",
-       date:"12/5/34"
-     },
-     {
-      id:nanoid(),
-       text:"hello jiju",
-       date:"12/5/34"
-     },
-     {
-      id:nanoid(),
-       text:"hello saala",
-       date:"12/5/34"
-     }
-
-
-
-  ]);
-
-
-  const deleteit=(id)=>{
-    const newnotes=notes.filter((note)=>
-    note.id!==id);
-
-    setNotes(newnotes);
-  }
-
-  
-  const[search,setSearch]=useState("");
-
- 
-
-
-  const addNote=(text)=>{
-
-    
-    const date=new Date();
-
-    const newnote={
-      id:nanoid(),
-      text:text,
-      date:date.toLocaleDateString(),
-      
-    
+    const deleteit = (id) => {
+        const newnotes = notes.filter((note) => note.id !== id);
+        setNotes(newnotes);
     }
 
-     const mynewnote=[...notes,newnote];
+    useEffect(() => {
+        localStorage.setItem('react-notes-app-data', JSON.stringify(notes));
+    }, [notes]);
 
-     setNotes(mynewnote);
 
-  }
 
-  return(
-    <div className="container">
-      <Header/>
-     <Search handleSearch={setSearch}/>
-     <NotesList notes={notes.filter((note)=>note.text.toLowerCase().includes(search))} handleAddNote={addNote}  deleteIt={deleteit}/>
-     
-    </div>
-  )
+
+    const addNote = (text) => {
+        const date = new Date();
+        const newnote = {
+            id: nanoid(),
+            text: text,
+            date: date.toLocaleDateString(),
+        }
+        const mynewnote = [...notes, newnote];
+        setNotes(mynewnote);
+    }
+
+    return (
+        <div className="container">
+            <Header />
+            <Search handleSearch={setSearch} />
+            <NotesList
+                notes={notes.filter((note) => note.text.toLowerCase().includes(search))}
+                handleAddNote={addNote}
+                deleteIt={deleteit}
+            />
+        </div>
+    )
 }
 
 export default App;
+
+
+
